00d635115f3787c85792a0331d2d14d42b46f0f0
KitauraYuma <yuma.kitaura@tcs.com>
2026-06-12 13:29:57 +0900

    Update CalculatorTest.java

--- src/test/java/com/example/handson/CalculatorTest.java
+++ src/test/java/com/example/handson/CalculatorTest.java
@@ -1,10 +1,23 @@
 package com.example.handson;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+
 public class CalculatorTest {
   @Test
   public void testAdd() {
     Calculator calc = new Calculator();
     assertEquals(5, calc.add(2, 3));
+  }
+  @Test
+  public void testAddDecimalValues() {
+    Calculator calc = new Calculator();
+    assertEquals(4.7, calc.add(1.2, 3.5), 0.0001);
+  }
+  @Test
+  public void testAddThreeValues() {
+    Calculator calc = new Calculator();
+    assertEquals(6.0, calc.add(1.0, 2.0, 3.0));
   }
 }
